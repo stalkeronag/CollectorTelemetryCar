d44ebc4ca9695204cb979a33968ed4f9957bc15e
Kostan <6PwCnSEPh2NHrEA>
2023-10-07 09:48:37 +0300

    create new project

---
+++ TelemetryCar/Simulator/Interfaces/ISimulator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator.Interfaces
+{
+    internal interface ISimulator
+    {
+        public void Start();
+
+        public void Stop();
+    }
+}
---
+++ TelemetryCar/TelemetryCar/Interfaces/IReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelemetryCar.Interfaces
+{
+    internal interface IReader
+    {
+        public byte[] Read();
+
+        public int available { get; set; }
+    }
+}
